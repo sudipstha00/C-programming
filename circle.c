9c3ea4fe160185ac5c6fd56eff3b777e786e8014
Sudip Shrestha <sudipstha07@gmail.com>
2018-04-21 17:10:46 +0545

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    float r,area;
+    printf("enter the radius of circle:");
+    scanf("%f",&r);
+    area=(22.0/7)*r*r;
+    printf("the area of circle is :%f",area);
+    getch();
+}
